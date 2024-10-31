ae0f8bb1c616fa55123b8b8c4c930bedf409f1d7
alex <simedrea.alex@gmail.com>
2024-10-31 16:31:46 +0200

    Initial Commit

---
+++ SchoolHub/SchoolHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolHubApp.swift
+//  SchoolHub
+//
+//  Created by Alexandru Simedrea on 12.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SchoolHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
